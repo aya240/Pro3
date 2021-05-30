4fcbd0267b0d439abec19650176920ce80d6f1c1
aya240 <ayamohsen223300@gmail.com>
2021-05-30 02:08:57 +0200

    asd

---
+++ pro3/build/desc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/am/Desktop/pro3/src/desc
+# Build directory: /home/am/Desktop/pro3/build/desc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
